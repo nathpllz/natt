e8d744125aac74b184a5d705db35de0a898784cb
nathpllz <161333624+nathpllz@users.noreply.github.com>
2024-10-04 08:21:21 -0300

    f

---
+++ main.js
@@ -0,0 +1,16 @@
+function soma(num1,num2){
+return num1+num2;
+}
+console.log(soma(5,3)):
+function mult(num1,num2){
+    return num1*num2;
+}
+function areaQuadrado(lado){
+    return lado**2;
+}
+console.log (areaQuadrado(6));
+let nome=valoresMedia=(prompt("qual sua nota do 1 tri?"));
+let media=function (x,y,z){
+    return(1+2+3)/3;
+}
+console.log(media(1+2+3/3));
