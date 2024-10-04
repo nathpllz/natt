6577bf493612d2ffb24b224209959a5f8b3a6072
nathpllz <161333624+nathpllz@users.noreply.github.com>
2024-10-04 08:29:45 -0300

    g

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-function soma(num1,num2){
-return num1+num2;
-}
-console.log(soma(5,3));
-function mult(num1,num2){
-    return num1*num2;
-}
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log (areaQuadrado(6));
-let nome=valoresMedia=(prompt("qual sua nota do 1 tri?"));
-let media=function (x,y,z){
-    return(1+2+3)/3;
-}
-console.log(media(1+2+3/3));
+// function soma(num1,num2){
+// return num1+num2;
+// }
+// console.log(soma(5,3));
+// function mult(num1,num2){
+//     return num1*num2;
+// }
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log (areaQuadrado(6));
+// let nome=valoresMedia=(prompt("qual sua nota do 1 tri?"));
+// let media=function (x,y,z){
+//     return(1+2+3)/3;
+// }
+// console.log(media(1+2+3/3));
+
+let estados= ['ac','al','am','pa','ba',
+    'df', 'es', 'go', 'ma', 'mt','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to'
+    ]
