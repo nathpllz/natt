771f20b1deeb8569e374b4570e50c79f4af76728
nathpllz <161333624+nathpllz@users.noreply.github.com>
2024-10-04 09:24:10 -0300

    v

--- main.js
+++ main.js
@@ -24,8 +24,11 @@ let estados= ['ac','al','am','pa','ba',
 // console.log(estados[i]);
 // i++;
 // }
-for (let j= 0; j<estados.length; j++) {
-console.log(j);
-   console.log(estados[j]);
-    
+// for (let j= 0; j<estados.length; j++) {
+// console.log(j);
+//    console.log(estados[j]);
+    let idade=17;
+if(idade>17){console.log('pode dirigir')}
+else{ console.log('ilegal dirigir')
+
 }
