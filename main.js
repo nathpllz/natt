b3b450440161de2e28a55bd822ff13d70f6712ee
nathpllz <161333624+nathpllz@users.noreply.github.com>
2024-10-04 09:02:01 -0300

    j

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ let estados= ['ac','al','am','pa','ba',
     'df', 'es', 'go', 'ma', 'mt','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to'
     ]
 let i= 0
-while(i<10){
+while(i<27){
 console.log(i);
 i++;
 console.log(estados[i]);
